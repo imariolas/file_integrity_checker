8d304addb58072ef98e40d04625ae5f8619f3548
imariolas <ignatis.mar128@gmail.com>
2025-03-27 02:28:50 +1100

    hash_directories method added to hash directories and subdirectories

--- src/core/hasher.py
+++ src/core/hasher.py
@@ -1,4 +1,5 @@
 import hashlib
+import pathlib
 
 def calculate_file_hashes(file_path, algorithms=None):
     
@@ -23,32 +24,61 @@ def calculate_file_hashes(file_path, algorithms=None):
     with open(file_path, 'rb') as f:
         chunk = f.read(8192)
         while chunk:
-                # update all hash objects with the chunk
-                for hash_obj in hash_objects.values():
-                    hash_obj.update(chunk)
-                chunk = f.read(8192)
+            # update all hash objects with the chunk
+            for hash_obj in hash_objects.values():
+                hash_obj.update(chunk)
+            chunk = f.read(8192)
     
     results = {algorithm: hash_obj.hexdigest() for algorithm, hash_obj in hash_objects.items()}
-    print(results)
+    #print(results)
     return results
 
+def hash_directories(directory_path, algorithms=None, recursive=True):
+    """
+        Calculate hashes for all files in a directory
 
-def sha256Hash():
-    h = hashlib.new("SHA256")
-    message = "Hello world!"
-    h.update(message.encode())
+        Args:
+            directory_path (str): path to the direcory
+            algorithms (list, optional): list of hash algorithms to use
+            recursive (bool, optional): whether to process subdirectories. defaults to True
 
-    msg_hash = h.hexdigest()
-    print(msg_hash)
+        Returns:
+            dict: Nested dictionary mapping file paths to their hash results
+    """
+    if algorithms is None: 
+        algorithms = ['md5', 'sha1', 'sha256']
 
-    h = hashlib.sha256()
-    message = "Hello world!"
-    h.update(message.encode())
+    directory_path = pathlib.Path(directory_path)
+    results = {}
 
-    msg_hash = h.hexdigest()
-    print(msg_hash)
+    # Determine which files to process
+    if recursive:
+        # Get all files in directory and subdirectories
+        files = [f for f in directory_path.glob('**/*') if f.is_file()]
+    else:
+        # Get only files in the furrent directory
+        files = [f for f in directory_path.glob('*') if f.is_file()]
+
+    
+    # Calculate hashes for each file
+    for file_path in files:
+        try:
+            # Use relative path as the key
+            rel_path = file_path.relative_to(directory_path)
+            results[str(rel_path)] = calculate_file_hashes(file_path, algorithms)
+          
+        except Exception as e:
+            results[str(file_path.relative_to(directory_path))] = f"Error: {str(e)}"
+    
+   #for r in results:
+      #  print(r)
+
+    print(results["tips"])
+    return results
+
 
 
 if __name__ == "__main__":
     #sha256Hash()
-    calculate_file_hashes("/home/lc/Desktop/tips", None)
+   # calculate_file_hashes("/home/lc/Desktop/tips", None)
+   hash_directories("/home/lc/Desktop/", None, False)
